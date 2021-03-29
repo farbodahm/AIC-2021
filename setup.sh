e06343be9d962e76a898135798aa10d417ed6231
mo1ein <moeinn.com@gmail.com>
2021-03-29 22:42:01 +0430

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,3 @@ cd client
 cp ./client ../../../server/
 cd ../../../server/
 sudo java -jar server-v1.2.4.jar --first-team=./client --second-team=./client
-cp ./log.json ../WebGlV1.0/
-cd ../WebGlV1.0
-python -m http.server --cgi 8360
