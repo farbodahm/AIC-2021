20213b7eb2d016fa4950cb1a401009b02eaf0ebb
mo1ein <moeinn.com@gmail.com>
2021-03-30 00:22:27 +0430

    get resource and put it to home

--- AIC21-Client-Cpp/client/src/AI/AI.cpp
+++ AIC21-Client-Cpp/client/src/AI/AI.cpp
@@ -5,11 +5,10 @@
 
 using namespace std;
 
-Answer *AI::turn(Game *game) {
-    Ant* me = game->getAnt();
-    cout << me->getHealth() << " " << me->getTeam() << endl;
+Answer *AI::turn(Game *game)
+{
 
-    int viewDist = me->getViewDistance();
+    /*
     string v = to_string(viewDist);
 
     string xx = to_string(me -> getX());
@@ -20,21 +19,44 @@ Answer *AI::turn(Game *game) {
         return new Answer(UP, all, 5);
     else
         return new Answer(DOWN, all, 5);
+    */
 
 
+    //CellType home;
 
-    /*
+    Ant* me = game->getAnt();
+    cout << me->getHealth() << " " << me->getTeam() << endl;
+
+    int hx = game -> getBaseX();
+    int hy = game -> getBaseY();
+
     int viewDist = me->getViewDistance();
+
+
     int targetX = -1, targetY = -1;
-    if (me->getType() == KARGAR) {
-        for (int i = 0; i < viewDist; i++) {
-            for (int j = 0; j < viewDist; j++) {
+
+    if (me->getType() == KARGAR)
+    {
+        for (int i = 0; i < viewDist; i++)
+        {
+            for (int j = 0; j < viewDist; j++)
+            {
                 Cell* cell = game->getAnt()->getNeighborCell(i, j);
-                if (cell->getResource()->getType() != NONE) {
+
+                if (me -> getType() == KARGAR && cell->getType() != WALL)
+                {
                     targetX = cell->getX();
                     targetY = cell->getY();
                     break;
                 }
+
+                /*
+                if (cell->getResource()->getType() == BASE)
+                {
+                    home = BASE;
+                }
+                */
+
             }
             if (targetX != -1)
                 break;
@@ -42,24 +64,44 @@ Answer *AI::turn(Game *game) {
     }
 
     if (targetX == -1) {
-        return new Answer(UP, "I'm searching up area!", 5);
+        return new Answer(UP, "bala", 5);
     }
 
     int x = me->getX();
     int y = me->getY();
+
     Direction direction = CENTER;
-    if (targetX > x) {
-        direction = RIGHT;
+
+
+    if(me -> getCurrentResource()-> getType() != NONE)
+    {
+        if (hx > x) {
+            direction = RIGHT;
+        }
+        else if (hx < x) {
+            direction = LEFT;
+        }
+        else if (hy > y) {
+            direction = DOWN;
+        }
+        else {
+            direction = UP;
+        }
+        return new Answer(direction, "I found a resource and I'm going to get it! :)", 10);
     }
-    else if (targetX < x) {
-        direction = LEFT;
+    else{
+        if (targetX > x) {
+            direction = RIGHT;
+        }
+        else if (targetX < x) {
+            direction = LEFT;
+        }
+        else if (targetY > y) {
+            direction = DOWN;
+        }
+        else {
+            direction = UP;
+        }
+        return new Answer(direction, "I found a resource and I'm going to get it! :)", 10);
     }
-    else if (targetY > y) {
-        direction = DOWN;
-    }
-    else {
-        direction = UP;
-    }
-    return new Answer(direction, "I found a resource and I'm going to get it! :)", 10);
-    */
 }
